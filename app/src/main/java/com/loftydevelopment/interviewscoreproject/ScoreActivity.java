cd02c0e99e2e7ba79828e09c00f9dd43858036d5
Connor Loftus <connorlof@gmail.com>
2019-03-26 18:25:24 -0400

    First Story: Single screen that displays score.

---
+++ app/src/main/java/com/loftydevelopment/interviewscoreproject/ScoreActivity.java
@@ -0,0 +1,13 @@
+package com.loftydevelopment.interviewscoreproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
